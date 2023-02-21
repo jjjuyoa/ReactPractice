eda2c734b6af612f0398d93bf3a2d337aaa66106
jjjuyoa <jjjuyoa@gmail.com>
2023-02-21 09:55:27 +0900

    first commit

---
+++ src/DiaryEditor.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
